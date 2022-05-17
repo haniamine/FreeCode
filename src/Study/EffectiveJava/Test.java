d351d2512a4bdf8f822f3c12b572b3727361416a
haniamine <haniamiine@gmail.com>
2022-05-17 22:28:47 +0100

    start books Java

---
+++ src/Study/EffectiveJava/Test.java
@@ -0,0 +1,9 @@
+package Study.EffectiveJava;
+
+import Utils.Print;
+
+public class Test {
+    public static void main(String[] args) {
+        Print.o("test");
+    }
+}
---
+++ src/Study/HeadFirstJava/Test.java
@@ -0,0 +1,9 @@
+package Study.HeadFirstJava;
+
+import Utils.Print;
+
+public class Test {
+    public static void main(String[] args) {
+        Print.o("test");
+    }
+}
